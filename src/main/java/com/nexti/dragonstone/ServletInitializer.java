e908c84d88b17508e5dcae8155126c06875c88f3
Cesar Martinez <poseidon24@hotmail.com>
2017-08-05 13:43:57 -0500

    Initial commit

---
+++ src/main/java/com/nexti/dragonstone/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nexti.dragonstone;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DragonstoneConsoleApplication.class);
+	}
+
+}
